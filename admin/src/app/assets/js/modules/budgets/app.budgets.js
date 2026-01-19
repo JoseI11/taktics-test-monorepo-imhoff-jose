f7765107c2eaff406921daab22acc30780de4e36
Jose Imhoff <jose-imhoff@hotmail.com>
2026-01-19 11:47:03 -0300

    feat(frontend): create budgets module with routes and controllers
    - Register budgets Angular module
    - Define routes: /budgets (list), /budgets/new (create), /budgets/:id (edit)
    - Import controllers and templates
    - Configure state transitions
    - Use /budgets/new before :id to avoid route conflicts"

--- admin/src/app/assets/js/modules/budgets/app.budgets.js
+++ admin/src/app/assets/js/modules/budgets/app.budgets.js
@@ -1,14 +1,32 @@
 import angular from 'angular';
 import budgetsUrl from './views/budgets.index.html';
+import budgetDetailUrl from './views/budgets.detail.html';
+
 import BudgetsController from './controllers/budgets.index.controller';
+import BudgetDetailController from './controllers/budgets.detail.controller';
 
 export default angular.module('app.budgets', []).config(routeConfig).name;
 
 function routeConfig($stateProvider) {
-  $stateProvider.state('budgets', {
-    url: '/budgets',
-    templateUrl: budgetsUrl,
-    controller: BudgetsController,
-    controllerAs: 'vm',
-  });
+  $stateProvider
+    .state('budgets', {
+      url: '/budgets',
+      templateUrl: budgetsUrl,
+      controller: BudgetsController,
+      controllerAs: 'vm',
+    })
+    .state('budgetCreate', {
+      url: '/budgets/new',  // Debe ir ANTES que :id
+      templateUrl: budgetDetailUrl,
+      controller: BudgetDetailController,
+      controllerAs: 'vm',
+    })
+    .state('budgetDetail', {
+      url: '/budgets/:id',
+      templateUrl: budgetDetailUrl,
+      controller: BudgetDetailController,
+      controllerAs: 'vm',
+    });
 }
+
+routeConfig.$inject = ['$stateProvider'];
